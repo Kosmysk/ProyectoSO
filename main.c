4c3ff47b6fa1b3b68d826482be53a23b81cb5af6
Felicia <feliciapino100@gmail.com>
2024-09-04 14:45:14 -0400

    Crear funcion miExec, para ejecutar comandos dado un arreglo de argumentos

--- main.c
+++ main.c
@@ -7,25 +7,42 @@
 para hacer la salida de uno la entrada del otro. Si args2 tambien contiene pipe,
 se va a llamar a si misma, recursivamente
 retorna 0 si se ejecuta correctamente y -1 si hay un error*/
-int recursivePipe(char* args1[], char* args2){
+int recursivePipe(char* args1[], char* args2[]){
   //TODO
   return 0;
 }
-
+int miExec(char* args[]){
+  int pid = fork();
+      if(pid==0){  
+        int e = execvp(args[0],args);
+        printf("hubo un error\n");
+      }else{
+        wait(pid);
+      }
+  return 0;
+}
 int main(){
   char input[1000];
   int argCount; 
-
+  
   while(1){
     printf("$ ");
     fgets(input, sizeof(input),stdin);//lee toda la linea de entrada
-    char* execArgs[] = {input};
+    input[strlen(input)-1]='\0';
+
+    //asumiendo input es "ls -l"
+    char* execArgs[] = {input,NULL};
     //TODO: separar execArgs debe tener los valores de input separados por espacio
     // ej: "aaa bbb ccc" -> {"aaa","bbb","ccc"}
     //TODO argCount debe guardar la cantidad de argumentos que tiene el comando ingresado
-
-    if(strcmp(execArgs[0],"exit\n")==0){
-      break;
+    
+    if(strcmp(execArgs[0],"exit")==0){
+      return 0;
+    }
+    if(/*no hay pipe*/1){ //TODO:hacer argumento del if
+      miExec(execArgs);
+    }else{
+      //si si hay pipe
     }
   }
   return 0;
