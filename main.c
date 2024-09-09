2d95436c13f1a8a19bf8541e96f530e3cabe437c
Felicia <feliciapino100@gmail.com>
2024-09-09 00:36:18 -0300

    quitar pedazos de codigo y fuciones que ya no se van a usar

--- main.c
+++ main.c
@@ -7,26 +7,8 @@
 #include <errno.h>
 #include <string.h>
 
-/*esta funcion va a tomar dos arreglos de argumentos, y usara la funcion pipe
-para hacer la salida de uno la entrada del otro. Si args2 tambien contiene pipe,
-se va a llamar a si misma, recursivamente
-retorna 0 si se ejecuta correctamente y -1 si hay un error*/
-int recursivePipe(char* args1[], char* args2[]){
-  //TODO
-  return 0;
-}
-/*
-int miExec(char* args[]){
-  int pid = fork();
-      if(pid==0){  
-        int e = execvp(args[0],args);
-        printf("hubo un error\n");
-      }else{
-        wait(pid);
-      }
-  return 0;
-}
-*/
+
+
 
 // Esta funcion, por medio de punteros, modificara el valor de el arreglo de strings execArgs[], y devolverá la cantidad de argumentos
 int separarTexto(char input[1000], char *argumentos[]){
@@ -120,16 +102,6 @@ int main(){
     //TODO: free comandos
     separarComandos(CantidadArgs, execArgs,cantidadComandos, comandos);
     
-    /*
-    for(int i=0;i<cantidadComandos;i++){//debug
-      printf("cmd %d: ",i+1);
-      for(int j=0; comandos[i][j]!=NULL; j++){
-        printf("%s ",comandos[i][j]);
-      }
-      printf("\n");
-    }
-    */
-    
 
     int p1[2];
     int p2[2];
@@ -168,13 +140,6 @@ int main(){
         swapPipes(p1,p2);
         
     }
-    /*
-    if(no hay pipe1){ //TODO:hacer argumento del if
-      miExec(execArgs);
-    }else{
-      //si si hay pipe
-    }
-    */
   }
   return 0;
 }
